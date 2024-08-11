11c6605c5cc548e039bae47f7b6deba5aee6cb12
salah mohamed <salah.mohamed@miu.edu>
2024-08-10 23:22:14 -0500

    [Add] admin panel react app

---
+++ admin-panel/src/config/sidebarNav.ts
@@ -0,0 +1,28 @@
+const sidebarNav = [
+  {
+    link: "/",
+    section: "dashboard",
+    icon: "lucide:layout-dashboard", //width:"20"
+    text: "Dashboard",
+  },
+  {
+    link: "/products",
+    section: "products",
+    icon: "icon-park-outline:ad-product",
+    text: "Products",
+  },
+  {
+    link: "/customers",
+    section: "customers",
+    icon: "ph:users-bold",
+    text: "Customers",
+  },
+  {
+    link: "/orders",
+    section: "orders",
+    icon: "icon-park-outline:transaction-order",
+    text: "Orders",
+  },
+];
+
+export default sidebarNav;
