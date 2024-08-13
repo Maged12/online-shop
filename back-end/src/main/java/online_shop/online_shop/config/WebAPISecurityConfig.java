189691162459b91ad6d8a4a8ca66877f775ca26e
Maged Rashed <engmagedr@gmail.com>
2024-08-12 23:13:15 -0500

    open cors for admin app

--- back-end/src/main/java/online_shop/online_shop/config/WebAPISecurityConfig.java
+++ back-end/src/main/java/online_shop/online_shop/config/WebAPISecurityConfig.java
@@ -102,7 +102,8 @@ public class WebAPISecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Update with your frontend URL
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3001"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow credentials like cookies
