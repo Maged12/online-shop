05dc3d84d2bf80a4e3a0d1281aa6791ce4c56e30
Maged Rashed <engmagedr@gmail.com>
2024-08-17 20:05:42 -0500

    finish project

--- admin-panel/src/components/topnav/rightBox/profile/Profile.tsx
+++ admin-panel/src/components/topnav/rightBox/profile/Profile.tsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import { images } from "../../../../constants";
 import classes from "./Profile.module.scss";
 import { useTranslation } from "react-i18next";
+import { useUser } from "../../../../store/UserContext";
 
 function Profile() {
   const { t } = useTranslation();
+  const { user } = useUser();
 
   return (
     <div className={classes.profile}>
@@ -11,7 +14,7 @@ function Profile() {
         <img src={images.avt} alt="avatar" />
       </div>
       <div className={classes.profile__info}>
-        <p className={classes.profile__userName}>{t("zahraMirzaei")}</p>
+        <p className={classes.profile__userName}>{t(user?.name || "")}</p>
         {/* <span className={classes.profile__role}>{t("admin")}</span> */}
       </div>
     </div>
--- back-end/src/main/java/online_shop/online_shop/OnlineShopApplication.java
+++ back-end/src/main/java/online_shop/online_shop/OnlineShopApplication.java
@@ -13,80 +13,9 @@ import online_shop.online_shop.property.FileStorageProperties;
 @EntityScan(basePackages = "online_shop.online_shop.domain")
 @EnableConfigurationProperties({ FileStorageProperties.class })
 public class OnlineShopApplication {
-	// @Autowired
-	// private UserRepository userRepository;
-
-	// @Autowired
-	// private ProductRepository productRepository;
-
-	// @Autowired
-	// private CategoryRepository categoryRepository;
-	// @Autowired
-	// private CartRepository cartRepository;
-	// @Autowired
-	// private OrderRepository orderRepository;
-	// @Autowired
-	// private CartItemRepository cartItemRepository;
-	// @Autowired
-	// private OrderItemRepository orderItemRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShopApplication.class, args);
 	}
 
-	// @Override
-	// public void run(String... args) throws Exception {
-	// // clean database
-	// orderItemRepository.deleteAll();
-	// orderRepository.deleteAll();
-	// cartItemRepository.deleteAll();
-	// cartRepository.deleteAll();
-	// productRepository.deleteAll();
-	// categoryRepository.deleteAll();
-	// userRepository.deleteAll();
-
-	// // Create and save a new User
-	// User user = new User("John Doe", "john@example.com", "password123",
-	// Role.CUSTOMER);
-	// userRepository.save(user);
-
-	// // Create and save a new Category
-	// Category electronics = new Category("Electronics", "Devices and gadgets");
-	// categoryRepository.save(electronics);
-
-	// // Create and save a new Product
-	// Product phone = new Product("Smartphone", "Latest model smartphone", 699.99,
-	// electronics, null);
-	// productRepository.save(phone);
-
-	// // Create and save a new Cart and CartItem
-	// Cart cart = new Cart(user, 0);
-	// CartItem cartItem = new CartItem(cart, phone, 1, phone.getPrice());
-	// cart.addItem(cartItem);
-	// cartRepository.save(cart);
-
-	// // Update Cart total price
-	// cart.setTotalPrice(phone.getPrice());
-	// cartRepository.save(cart);
-
-	// // Create and save a new Order and OrderItem
-	// Order order = new Order(new Date(), "PENDING", cart.getTotalPrice(), user);
-	// OrderItem orderItem = new OrderItem(phone, order, 1, phone.getPrice());
-	// order.addItem(orderItem);
-	// orderRepository.save(order);
-
-	// // Retrieve and print all users
-	// System.out.println("All Users:");
-	// userRepository.findAll().forEach(System.out::println);
-
-	// // Retrieve and print all products
-	// System.out.println("All Products:");
-	// productRepository.findAll().forEach(System.out::println);
-
-	// // Retrieve and print all orders
-	// System.out.println("All Orders:");
-	// orderRepository.findAll().forEach(System.out::println);
-	// productRepository.save(phone);
-
-	// }
 }
